4c92d3ed8dd285c27c484acfa72d83ccf15b4902
林志杰 <1240078249@qq.com>
2020-11-13 12:53:30 +0800

    上传了该项目的所有文件

---
+++ html+css练习-品优购项目/js/lift.js
@@ -0,0 +1,59 @@
+
+//JQuery入口函数
+$(function(){
+	var flag = true
+	let toolTop = $(".recom").offset().top
+	toggleTool()
+	
+	//滑动出现电梯导航栏
+	function toggleTool(){
+		if($(document).scrollTop() >= toolTop){
+			$(".fixedtool").fadeIn()
+		}else {
+			$(".fixedtool").fadeOut()
+		}
+	}
+	
+	$(window).scroll( function(){
+		toggleTool()
+		
+		if(flag){
+		$(".floor .w").each(function(index,ele){
+			if($(document).scrollTop() >= $(ele).offset().top){
+				$(".fixedtool li").eq(index).addClass("current").siblings().removeClass();
+			}
+		})
+		}
+	})
+	
+	
+	//电梯导航栏点击事件
+	$(".fixedtool li").click(function(){
+		flag = false
+		var current = $(".floor .w").eq($(this).index()).offset().top
+		$("body, html").stop().animate({
+			scrollTop: current
+		},function(){
+			flag = true
+		})
+		$(this).addClass("current").siblings().removeClass()
+	})
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+})
